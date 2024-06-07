b47728e6c2da2490f55ac99f18e338c7bf36b404
felipewiiu3 <felipewiiu3@gmail.com>
2024-06-07 12:20:35 -0300

    Realiza a liberação da rota de documentação swagger

--- src/main/java/com/company/reverselog/configuration/CorsConfiguration.java
+++ src/main/java/com/company/reverselog/configuration/CorsConfiguration.java
@@ -11,6 +11,9 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+        ;
     }
 }
